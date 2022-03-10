20060fd1fd763740fbc8e42aab8391dcc2ddbe91
DevNotHacker <85191790+DevNotHacker@users.noreply.github.com>
2022-03-10 16:52:29 +0000

    clicker js cookies improved

--- earn/clicker.js
+++ earn/clicker.js
@@ -1,3 +1,18 @@
+function getCookie(c_name)
+{
+  if (document.cookie.length>0)
+  {
+    c_start = document.cookie.indexOf(c_name + "=");
+    if (c_start!=-1)
+    {
+      c_start = c_start + c_name.length+1;
+      c_end = document.cookie.indexOf(";",c_start);
+      if (c_end==-1) c_end = document.cookie.length;
+      return unescape(document.cookie.substring(c_start,c_end));
+    }
+  }
+  return "";
+}
 function findcookie(type, str="", ifstat = true) {
 	if (type==="all") {
 		return (document.cookie);
@@ -8,3 +23,4 @@ function findcookie(type, str="", ifstat = true) {
 	}
 }
 let cookies = findcookie("all");
+findcookie("other", str="points=0", ifstat=("points" in cookies))
